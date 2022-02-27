e26e453a117f7e17693d5234743d19d42e4f5272
yetistiren <hakan@yetistiren.net>
2022-02-27 19:07:34 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,79 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int dogumYili, kalan;
+        String burc = "";
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Doğum Yılınızı Giriniz : ");
+        dogumYili = input.nextInt();
+        kalan = dogumYili % 12;
+
+        switch (kalan) {
+            case 0: {
+                burc = "Maymun";
+                break;
+            }
+            case 1: {
+                burc = "Horoz";
+                break;
+            }
+            case 2: {
+                burc = "Köpek";
+                break;
+            }
+            case 3: {
+                burc = "Domuz";
+                break;
+            }
+            case 4:
+            {
+                burc = "Fare";
+                break;
+            }
+            case 5:
+            {
+                burc = "Öküz";
+                break;
+            }
+            case 6:
+            {
+                burc = "Kaplan";
+                break;
+            }
+            case 7:
+            {
+                burc = "Tavşan";
+                break;
+            }
+            case 8:
+            {
+                burc = "Ejderha";
+                break;
+            }
+            case 9:
+            {
+                burc = "Yılan";
+                break;
+            }
+            case 10:
+            {
+                burc = "At";
+                break;
+            }
+            case 11:
+            {
+                burc = "Koyun";
+                break;
+            }
+        }
+        
+        System.out.print("Çin Zodyağı Burcunuz : " + burc);
+    }
+}
